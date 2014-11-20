439e64fb330e6ba8bd5392bd4d62571cff5d0193
Johnny Pesola <jp222px@student.lnu.se>
2014-11-20 19:05:39 +0000

    Assignment 1c rewritten

--- Uppgift 1c/js/uppgift1c.js
+++ Uppgift 1c/js/uppgift1c.js
@@ -3,41 +3,50 @@
 function NastStorsta()
 {
     var formObj = document.getElementById("user-input-form");
+    var inputContainer = document.getElementById("input-container");
+    var numberOfInputs = 10;
+
+    // Lägg till önskat många inputs i formen
+    for(var i = 0; i < numberOfInputs; i++)
+    {
+        inputContainer.appendChild(document.createElement("input"));
+    }
     
     formObj.onsubmit = function(){
-
+        
         var outputString = "";
-        var inputValue = formObj.elements['user-input'].value;
-        var inputsArray;
-    
-        // Felhantering
-        if(  inputValue == '' ||
-            (inputValue.match(/[^\d\s\-]|\-[^\d]/) || []).length > 0 ||
-             inputValue[inputValue.length-1] == "-"){
-            
-            alert("Var god skriv tio heltal separerade med mellanslag.");
-            return false;
-        }
+        var inputs = inputContainer.getElementsByTagName("input");
 
-        // Skapar array av godtyckligt antal nummer
-        inputsArray = inputValue.trim().split(/\s+/);
-        
-        // Kollar om man har angett minst två värden
-        if(inputsArray.length !== 10){
-            alert("Du måste ange tio heltal");
-            return false;
+        var highestValue;
+        var nextHighestValue;
+
+        // Går igenom alla inputs
+        for(var i = 0; i < inputs.length; i++){
+            // Felhantering
+            if(  inputs[i].value === '' ||
+                (inputs[i].value.match(/^-?[1-9]\d*$/) || []).length === 0){
+                
+                alert("Var god skriv totalt " + numberOfInputs + " heltal i varsit fält.");
+                return false;
+            }
+            
+            // Tar reda på om värdet är högst
+            if(+inputs[i].value >= +highestValue || typeof(highestValue) == "undefined"){
+                nextHighestValue = highestValue;
+                highestValue = inputs[i].value;
+            }
+            // Om inte värdet var högst, tar reda på om det är näst högst
+            else if(+inputs[i].value >= +nextHighestValue || typeof(nextHighestValue) == "undefined"){
+               nextHighestValue = inputs[i].value;
+            }
+
         }
         
-        // Sorterar värdena efter numrets värde
-        inputsArray.sort(function(a, b){
-          return a - b;
-        });
-        
-        outputString = "Det nästhögsta värdet " + inputsArray[inputsArray.length-2];
+        console.log(highestValue + " This is higher than " + nextHighestValue);
 
         // Presentera resultatet för användaren
         document.getElementById('output').innerHTML = outputString;
-        
+
         // Hindrar formen från att postas
         return false;
     }
