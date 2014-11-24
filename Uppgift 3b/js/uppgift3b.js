57eb9e38bcf2da84e15f5b31b94910306d5b3251
Johnny Pesola <jp222px@student.lnu.se>
2014-11-24 01:37:29 +0000

    Better test output

--- Uppgift 3b/js/uppgift3b.js
+++ Uppgift 3b/js/uppgift3b.js
@@ -21,16 +21,16 @@ function exec()
         }
         
         // Addera fraction objekt
-        output += "<div>Ditt bråktal adderad med bråktalet 6/3 blir " + myFraction1.add(myFraction2).toString() + "</div>";
+        output += "<div>Ditt bråktal adderad med bråktalet 6/3 blir: <strong>" + myFraction1.add(myFraction2).toString() + "</strong></div>";
         
         // Multiplicera fraction object
-        output += "<div>Ditt bråktal multiplicerad med bråktalet 6/3 blir " + myFraction1.multiply(myFraction2).toString() + "</div>";
+        output += "<div>Ditt bråktal multiplicerad med bråktalet 6/3 blir: <strong>" + myFraction1.multiply(myFraction2).toString() + "</strong></div>";
         
         // Om bråktalsobjektet är negativt
-        output += "<div>Om ditt bråktal är negativt: " + myFraction1.isNegative() + "</div>";
+        output += "<div>Om ditt bråktal är negativt: <strong>" + myFraction1.isNegative() + "</strong></div>";
         
         // Om bråktalsobjekten är lika
-        output += "<div>Ditt bråktal har samma värden som bråktalsobjektet med värdena 6/3: " + myFraction1.isEqualTo(myFraction2) + "</div>";
+        output += "<div>Ditt bråktal har samma värden som bråktalsobjektet med värdena 6/3: <strong>" + myFraction1.isEqualTo(myFraction2) + "</strong></div>";
 
         document.getElementById("output").innerHTML = output;
         
